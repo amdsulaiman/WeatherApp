cc8c39678db5eeeaca575a482240a094d68cd743
MohammedSulaiman4046 <amdsulaiman17@gmail.com>
2021-11-09 21:37:08 +0530

    Update API.swift

--- WeatherApp/Networking/API.swift
+++ WeatherApp/Networking/API.swift
@@ -2,11 +2,10 @@
 //  API.swift
 //  WeatherApp
 //
-//  Created by Gourav on 14/02/21.
 //
 
 import Foundation
 
 struct API {
-   static var key = "bc4f99db2a2270f98a1ad97d248f7a8e" //this is fake key use your own key
+   static var key = "Your API Key" //this is fake key use your own key
 }
